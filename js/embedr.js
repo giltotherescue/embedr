ba7ac508fc20756cbea1b9830925d091d9b2c002
Gil Hildebrand <gilhildebrand@gmail.com>
2012-05-04 16:02:40 -0400

    Cleanup

--- js/embedr.js
+++ js/embedr.js
@@ -11,8 +11,6 @@ searchTweets = function(term, twitter_user) {
   console.log ('Searching for "' + term + '" tweets from ' + twitter_user);
   var twitter_api_url = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name='+twitter_user+'&count=200&trim_user=true&callback=?';
 
-  var url_map = [];
-
   // Enable caching
   $.ajaxSetup({ cache: true });
 
@@ -24,11 +22,8 @@ searchTweets = function(term, twitter_user) {
     function(data) {
       console.log("Retrieved " + data.length + " tweets");
       $.each(data, function(i, tweet) {
-        // Uncomment line below to show tweet data in Fire Bug console
-        // Very helpful to find out what is available in the tweet objects
         // console.log(tweet);
 
-        // Before we continue we check that we got data
         if (tweet.text !== undefined) {
           lower_term = term.toLowerCase();
           if (tweet.text.toLowerCase().indexOf(lower_term) == -1) {
@@ -36,24 +31,13 @@ searchTweets = function(term, twitter_user) {
           }
 
           var this_tweet_id = tweet.id_str;
-
-          // Calculate how many hours ago was the tweet posted
-          var date_tweet = new Date(tweet.created_at);
-          var date_now   = new Date();
-          var date_diff  = date_now - date_tweet;
-          var hours      = Math.round(date_diff/(1000*60*60));
-
           var exp = /(\bhttp:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
           var urls = tweet.text.match(exp);
-
           var tweet_linked = replaceURLWithHTMLLinks(tweet.text);
 
           if (urls && urls.length) {
-
             // Build the html string for the current tweet
             var tweet_html = '<div class="span3" style="display: none"><div id="tweet'+tweet.id_str+'" class="tweet_text">';
-            //tweet_html    += '<a href="http://www.twitter.com/';
-            //tweet_html    += twitter_user + '/status/' + tweet.id + '">';
             tweet_html    += tweet_linked + '</div></div>';
 
             // Append html string to tweet_container div
@@ -75,17 +59,9 @@ searchTweets = function(term, twitter_user) {
               });
             });
           }
-
-          /*$('#tweet_container').embedly({
-            maxWidth: 450,
-            wmode: 'transparent',
-            method: 'afterParent',
-            key:'6fa2df1e93ec11e1b4f54040aae4d8c9'
-          });*/
         }
       });
       $('#tweet_container .loading').remove();
-      //$('#pinboard').pinboard();
     }
   );
 };
